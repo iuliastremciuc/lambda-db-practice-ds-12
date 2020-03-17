4f04fa528f00f9ea5cafeff71c4522b8d950171e
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-17 14:14:39 -0400

    Approach 2 is working

--- app/pg_queries.py
+++ app/pg_queries.py
@@ -49,30 +49,29 @@ print("RESULT:", len(result))
 #"""
 
 #
-# APPROACH 2 (needs updating / wasn't working sorry!)
+# APPROACH 2
 #
-#my_dict = { "a": 1, "b": ["dog", "cat", 42], "c": 'true' }
-#insertion_query = f"INSERT INTO test_table (name, data) VALUES (%s, %s)"
-#cursor.execute(insertion_query,
-#  ('A rowwwww', 'null')
-#)
-#cursor.execute(insertion_query,
-#  ('Another row, with JSONNNNN', json.dumps(my_dict))
-#)
-#cursor.execute(insertion_query)
+my_dict = { "a": 1, "b": ["dog", "cat", 42], "c": 'true' }
+insertion_query = "INSERT INTO test_table (name, data) VALUES (%s, %s)"
+cursor.execute(insertion_query,
+    ('A rowwwww', 'null')
+)
+cursor.execute(insertion_query,
+  ('Another row, with JSONNNNN', json.dumps(my_dict))
+)
 
 #
 # APPROACH 3 (multi-row insert!)
 #
 
-my_dict = { "a": 1, "b": ["dog", "cat", 42], "c": 'true' }
-# h/t: https://stackoverflow.com/questions/8134602/psycopg2-insert-multiple-rows-with-one-query
-insertion_query = "INSERT INTO test_table (name, data) VALUES %s"
-execute_values(cursor, insertion_query, [
-  ('A rowwwww', 'null'),
-  ('Another row, with JSONNNNN', json.dumps(my_dict)),
-  ('Third row', "3")
-])
+#my_dict = { "a": 1, "b": ["dog", "cat", 42], "c": 'true' }
+## h/t: https://stackoverflow.com/questions/8134602/psycopg2-insert-multiple-rows-with-one-query
+#insertion_query = "INSERT INTO test_table (name, data) VALUES %s"
+#execute_values(cursor, insertion_query, [
+#  ('A rowwwww', 'null'),
+#  ('Another row, with JSONNNNN', json.dumps(my_dict)),
+#  ('Third row', "3")
+#])
 
 cursor.execute("SELECT * from test_table;")
 result = cursor.fetchall()
