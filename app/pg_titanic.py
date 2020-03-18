0b4f4ece0cd761e2824ed5876533cc9e5de460ee
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-18 12:47:38 -0400

    Data pipeline, with help from friends

--- app/pg_titanic.py
+++ app/pg_titanic.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import execute_values
+import pandas
 
 load_dotenv() # look in the .env file for env vars, and add them to the env
 
@@ -24,7 +25,7 @@ print("CURSOR:", cursor)
 query = """
 CREATE TABLE IF NOT EXISTS passengers (
   id SERIAL PRIMARY KEY,
-  survived bool,
+  survived int,
   pclass int,
   name varchar,
   sex varchar,
@@ -43,9 +44,23 @@ print("PASSENGERS:", len(result))
 if len(result) == 0:
     # INSERT RECORDS
 
+    #CSV_FILEPATH = "data/titanic.csv"
+    CSV_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "data", "titanic.csv")
+    print("FILE EXISTS?", os.path.isfile(CSV_FILEPATH))
+    df = pandas.read_csv(CSV_FILEPATH)
+    print(df.head())
 
+    # rows should be a list of tuples
+    # [
+    #   ('A rowwwww', 'null'),
+    #   ('Another row, with JSONNNNN', json.dumps(my_dict)),
+    #   ('Third row', "3")
+    # ]
+    # h/t Jesus and https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.itertuples.html
+    rows = list(df.itertuples(index=False, name=None))
 
-
+    insertion_query = "INSERT INTO passengers (survived, pclass, name, sex, age, sib_spouse_count, parent_child_count, fare) VALUES %s"
+    execute_values(cursor, insertion_query, rows)
 
 
 # ACTUALLY SAVE THE TRANSACTIONS
